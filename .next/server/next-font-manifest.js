bc493625cf21bee7d4d477a69aa41382992eb404
Vi <krausz.st@gmail.com>
2024-08-26 11:42:54 +0400

    languages fix

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/app/page\": []\n  },\n  \"appUsingSizeAdjust\": false,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
-=======
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/app/page\": [],\n    \"[project]/app/crypto-casinos/page\": []\n  },\n  \"appUsingSizeAdjust\": false,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
->>>>>>> 651f75eb693417d481e671ae611b8de924b122e4
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/FFX8PGJXxdDVfX1NsrCQ9ZxrIw0NmJwq/VYV+WzhYw=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0K3LH5PynoMNHTNt+Hk2Az6psi+Otul3YEeIS2mRjeQ=\"\n}"
->>>>>>> 651f75eb693417d481e671ae611b8de924b122e4
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4G3dXefTEsveLNXzP0tiKYcm9nNSazggrfry/IImvGM=\"\n}"
