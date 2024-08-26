4e00f39a6a3644fff9c6020e425032f00060e47b
Oleh <gend4016@gmail.com>
2024-08-26 17:34:40 +0300

    a

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1/uxlty+ro/+KtdlrTChujXnPv5hwx0wPhPD+YTkJRw=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4G3dXefTEsveLNXzP0tiKYcm9nNSazggrfry/IImvGM=\"\n}"
->>>>>>> 3e3fee7a83b8607008a1c4aab88f0501f36822c3
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"LDEl1OCs76TBmb1YX0fTr5jNANP2uNdQ+kqhoXkb2GU=\"\n}"
