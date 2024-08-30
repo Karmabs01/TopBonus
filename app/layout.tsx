d260ab9079bb78c9452051699cbf463f16483d26
Vi <krausz.st@gmail.com>
2024-08-30 11:30:23 +0400

    Gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,14 +35,14 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M68KW5XM');
+})(window,document,'script','dataLayer','G-BTGRSY2322');
           `}
         </Script>
       </head>
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M68KW5XM"
+            src="https://www.googletagmanager.com/ns.html?id=G-BTGRSY2322"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
