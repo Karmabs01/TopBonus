854f2f651cbf5093414b087bb4b4f0be67a65179
Oleh <gend4016@gmail.com>
2024-09-03 13:07:41 +0300

    a

--- components/Banners_tailwind/Fortune_wheel_banner/index.jsx
+++ components/Banners_tailwind/Fortune_wheel_banner/index.jsx
@@ -4,8 +4,7 @@ import Link from "next/link";
 import { useTranslation } from "react-i18next";
 import "./styled.component.css";
 import Brands_carousel from "@/components/Banners_tailwind/Brands_carousel";
-import { useEffect, useRef, useState } from "react";
-import Loader from "@/components/Loader"; // Убедитесь, что у вас есть компонент Loader
+import { useEffect, useRef } from "react";
 
 export default function Banner_small() {
   const { t } = useTranslation();
@@ -14,7 +13,6 @@ export default function Banner_small() {
   const creative = "FW_Brands_Main_Page";
 
   const elementRef = useRef(null);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const hash = window.location.hash;
@@ -37,15 +35,11 @@ export default function Banner_small() {
               if (Math.abs(currentScrollY - elementPosition) > 1) {
                 // Если не достигнуто, повторяем попытку
                 scrollAttempt();
-              } else {
-                setLoading(false); // Скрываем Loader, когда скролл завершен
               }
             });
           };
 
           scrollAttempt();
-        } else {
-          setLoading(false); // Если элемента нет, скрываем Loader
         }
       };
 
@@ -53,15 +47,12 @@ export default function Banner_small() {
       setTimeout(() => {
         scrollToElement();
       }, 1500); // Увеличьте значение задержки, если требуется больше времени для рендеринга
-    } else {
-      setLoading(false); // Если нет хэша, сразу отключаем загрузку
     }
   }, []);
 
   return (
     <>
-      {loading && <Loader />}
-      <div ref={elementRef} id="fortune-wheel" className={`mt-10 mtt mmt-mob ${loading ? 'hidden' : ''}`}>
+      <div ref={elementRef} id="fortune-wheel" className="mt-10 mtt mmt-mob">
         <div className="main__container bg-main">
           <div className="background overflow-hidden background-roulete rounded-xl h-full">
             <div className="mx-auto max-w-7xl px-6 lg:px-8 flex flex-col">
