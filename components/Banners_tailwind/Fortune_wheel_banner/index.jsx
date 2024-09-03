fad04f56a99455e5210ee183688ce58a9ad27d2e
Oleh <gend4016@gmail.com>
2024-09-03 12:29:50 +0300

    a

--- components/Banners_tailwind/Fortune_wheel_banner/index.jsx
+++ components/Banners_tailwind/Fortune_wheel_banner/index.jsx
@@ -18,18 +18,44 @@ export default function Banner_small() {
     const hash = window.location.hash;
 
     if (hash && elementRef.current) {
-      // Используем requestAnimationFrame для более надёжной прокрутки
-      const scrollToElement = () => {
+      // Создаем MutationObserver для отслеживания изменений в DOM
+      const observer = new MutationObserver((mutationsList, observer) => {
+        let allComponentsLoaded = true;
+
+        // Проверяем, все ли элементы загружены
+        for (const mutation of mutationsList) {
+          if (mutation.type === "childList" || mutation.type === "subtree") {
+            allComponentsLoaded = false;
+            break;
+          }
+        }
+
+        if (allComponentsLoaded) {
+          // Выполняем скролл к элементу после завершения всех изменений
+          const id = hash.substring(1); // Убираем первый символ "#" из строки
+          const element = document.getElementById(id);
+          if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+          }
+
+          // Отключаем observer, так как он больше не нужен
+          observer.disconnect();
+        }
+      });
+
+      // Начинаем наблюдение за изменениями в DOM
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+      });
+
+      // Задержка для рендеринга других компонентов
+      setTimeout(() => {
         const id = hash.substring(1); // Убираем первый символ "#" из строки
         const element = document.getElementById(id);
         if (element) {
           element.scrollIntoView({ behavior: "smooth" });
         }
-      };
-
-      // Задержка для завершения рендеринга других компонентов
-      setTimeout(() => {
-        requestAnimationFrame(scrollToElement);
       }, 100); // Можно настроить это значение в зависимости от сложности страницы
     }
   }, []);
